bdaeefd13fa4487bb6b70c4e4643b2e41e597af6
Pulkit Garg <pulkitgarg2014@gmail.com>
2018-06-30 21:18:19 -0500

    Visualization added

--- StockPricePredictor.py
+++ StockPricePredictor.py
@@ -5,6 +5,9 @@ Created on Sat Jun 30 16:46:35 2018
 @author: Pulkit Garg
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import csv
 
 print('A program to predict if the price of the stock will go up or not')
@@ -18,20 +21,20 @@ print('5. Walmart')
 print('6. General Motors')
 
 choice = input("Please choose one of the above ")
-
-
-
-# initializing the titles and rows list
-fields = []
-rows = []
  
-# reading csv file
-with open("Google Stock Market Data.csv", 'r') as csvfile:
-    # creating a csv reader object
-    csvreader = csv.reader(csvfile)
-    for row in reader:
-        print(" ".join(row))
-
+data = pd.read_csv('Google Stock Market Data.csv')
+print(data)
+data.head()
+data['Date'] = pd.to_datetime(data['Date'].values)
+date = data['Date'].values
+price = data['Close'].values
+plt.plot(date, price)
+plt.title("Stock's closing price over time")
+plt.xlabel('Year')
+plt.ylabel('Price in USD')
+plt.legend(['Google'])
+plt.axis(['2004', '2015', 0, 1300])
+plt.show()
 
 
 
